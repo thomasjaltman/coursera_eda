f04aa5bc1aa8bb0f7278ec2715fd141a44fc2e72
Thomas <thomasjaltman@gmail.com>
2014-05-06 23:16:32 -0500

    makin graphs like all day

--- plot2.R
+++ plot2.R
@@ -1,5 +1,6 @@
 data <- read.csv("data.csv")
+data$dateTime <- strptime(data$dateTime, format="%Y-%m-%d %H:%M:%S")
 
 png("plot2.png")
-plot(data$Global_active_power, type="l")
+plot(data$dateTime, data$Global_active_power, type="l")
 dev.off()
